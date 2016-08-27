c2efd85c2c5b5fa2f67bbee1fd0f8c256839584b
weikun <wk2003119@163.com>
2016-08-27 16:40:37 +0800

    827

--- src/com/weikun/B/Animal.java
+++ src/com/weikun/B/Animal.java
@@ -4,5 +4,7 @@ package com.weikun.B;
  * Created by Administrator on 2016/8/27.
  */
 public class Animal {
-
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
 }
